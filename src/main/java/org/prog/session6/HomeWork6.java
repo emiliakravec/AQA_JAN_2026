c5d0e374f5de315c5eeaecdab68bc3bc506ba794
Emilia <emilikra1vec04@gmail.com>
2026-03-30 14:58:22 +0300

    HomeWork6

--- src/main/java/org/prog/session6/HomeWork6.java
+++ src/main/java/org/prog/session6/HomeWork6.java
@@ -1,4 +1,22 @@
 package org.prog.session6;
 
 public class HomeWork6 {
+
+    public String model;
+    public String name;
+    public String plateNumber;
+
+    public void setTruckInfo(String m, String n, String p) {
+        model = m;
+        name = n;
+        plateNumber = p;
+    }
+
+    public String getTruckInfo() {
+        return "Owner: " + name + ", Model: " + model + ", Plate: " + plateNumber;
+    }
+
+    public void delivery(String from, String to) {
+        System.out.println("Delivering " + from + " to " + to + " by " + name);
+    }
 }
--- src/main/java/org/prog/session6/Main.java
+++ src/main/java/org/prog/session6/Main.java
@@ -3,21 +3,28 @@ package org.prog.session6;
 public class Main {
 
     public static void main(String[] args) {
-//        int i = Calc.add(1, 10);
-//        String s = Calc.concat("1", "2");
+//       int i = Calc.add(1, 10);
+//       String s = Calc.concat("1", "2");
 //
 //        System.out.println(i);
-//        System.out.println(s);
+//       System.out.println(s);
 
-        System.out.println(Calc.countTo(20));
-        System.out.println(Calc.countTo(49));
-        System.out.println(Calc.countTo(100));
-        Calc.countFromTo(30, 60);
+//        System.out.println(Calc.countTo(20));
+//        System.out.println(Calc.countTo(49));
+//        System.out.println(Calc.countTo(100));
+//        Calc.countFromTo(30, 60);
 
 //        Truck truck1 = new Truck();
 //        truck1.name = "Vasya";
 //        truck1.delivery("Lviv", "Kyiv");
 //        truck1.delivery("Kyiv", "Odessa");
 //        truck1.delivery("Odessa", "Kyiv");
+        HomeWork6 hwTruck = new HomeWork6();
+
+        hwTruck.setTruckInfo("Volvo", "Emilia", "AA1234BB");
+
+        System.out.println(hwTruck.getTruckInfo());
+
+        hwTruck.delivery("Kyiv", "Lviv");
     }
 }
